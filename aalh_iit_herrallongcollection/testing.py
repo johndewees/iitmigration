41fe5463c9e32dbd92dd06be4d05963ea243869f
johndewees <john.dewees@gmail.com>
2020-12-11 15:42:43 -0500

    Add files via upload

---
+++ aalh_iit_herrallongcollection/testing.py
@@ -0,0 +1,3 @@
+oldstring = 'An undated portrait of an older man wearing a Florida seagull poop hat as he holds a basket of tomatoes at the Toledo Farmers Market in Downtown Toledo. Terms associated with the photograph are: Toledo Farmers&#39; Market (Ohio) | Farmers&#39; markets | Tomatoes | Older men | Portraits'
+newstring = oldstring.replace("&#39;", "'")
+print(newstring)
