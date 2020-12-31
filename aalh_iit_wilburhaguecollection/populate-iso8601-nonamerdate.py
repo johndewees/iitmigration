5a278be00a4fefc3c4f2e3e4b1e78c24163424ff
johndewees <john.dewees@gmail.com>
2020-12-31 16:27:12 -0500

    Add files via upload

---
+++ aalh_iit_wilburhaguecollection/populate-iso8601-nonamerdate.py
@@ -0,0 +1,39 @@
+from openpyxl import load_workbook
+import re
+
+filename = 'aalh_iit_wilburhaguecollection.xlsx'
+wb = load_workbook(filename)
+ws = wb['Metadata Template']
+
+minimumcol = 15
+maximumcol = 15
+minimumrow = 7
+maximumrow = 143
+
+iterationrow = 7
+targetcol = 15
+isostandardcol = 16
+
+for row in ws.iter_rows(min_row=minimumrow, min_col=minimumcol, max_row=maximumrow, max_col=maximumcol):
+    print(iterationrow)
+    for cell in row:
+        testvar = ws.cell(row=iterationrow, column=targetcol).value
+        print(testvar)
+        isovalue = None
+        try:
+            if testvar == None:
+                ws.cell(row=iterationrow, column=isostandardcol).value = ''
+                print('STATUS = NO DATE')
+            elif testvar.find('-') != -1:
+                isovalue = testvar
+                ws.cell(row=iterationrow, column=isostandardcol).value = isovalue
+            elif testvar.find(',') != -1:
+                print('STATUS = AMERICAN DATE')
+            else :
+                isovalue = re.findall('\d\d\d\d', testvar)
+                ws.cell(row=iterationrow, column=isostandardcol).value = isovalue[0]
+        except:
+            print('STATUS = PROBLEM')
+        print(ws.cell(row=iterationrow, column=isostandardcol).value)
+        iterationrow = iterationrow + 1
+wb.save('aalh_iit_wilburhaguecollection.xlsx')
