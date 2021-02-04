cf1df5fce2412039dad55b58f13c75335150f43a
johndewees <john.dewees@gmail.com>
2021-02-04 13:26:44 -0500

    Add files via upload

---
+++ aalh_iit_peopleportraits_003/cleanup-description-column.py
@@ -0,0 +1,28 @@
+from openpyxl import load_workbook
+
+filename = 'aalh_iit_peopleportraits_003.xlsx'
+wb = load_workbook(filename)
+ws = wb['Metadata Template']
+
+minimumcol = 8
+maximumcol = 8
+minimumrow = 7
+maximumrow = 506
+
+iterationrow = 7
+
+for row in ws.iter_rows(min_row=minimumrow, min_col=minimumcol, max_row=maximumrow, max_col=maximumcol):
+    for cell in row:
+        print(iterationrow)
+        iitdescription = ws.cell(row=iterationrow, column=minimumcol).value
+        if iitdescription == None:
+            print('No description')
+        elif iitdescription.endswith(','):
+            print(iitdescription)
+            description1 = iitdescription
+            description2 = description1[:-1]
+            description3 = description2 + '.'
+            ws.cell(row=iterationrow, column=minimumcol).value = description3
+            print(ws.cell(row=iterationrow, column=minimumcol).value)
+        iterationrow = iterationrow + 1
+wb.save('aalh_iit_peopleportraits_003.xlsx')
