d5fe6ef88bd638b0f9941952f2d1654c8cb49ccd
johndewees <john.dewees@gmail.com>
2021-02-11 17:51:39 -0500

    Add files via upload

---
+++ aalh_iit_buildings_04/cleanup-subjects-column.py
@@ -0,0 +1,81 @@
+from openpyxl import load_workbook
+
+filename = 'aalh_iit_buildings_04.xlsx'
+wb = load_workbook(filename)
+ws = wb['Metadata Template']
+
+minimumcol = 8
+maximumcol = 8
+minimumrow = 7
+maximumrow = 529
+
+iterationrow = 7
+targetcol = 9
+placecol = 13
+
+subjectholland = 'Images in time photographic collection. (Toledo Lucas County Public Library); Holland (Ohio). History. Photographs.'
+subjectwaterville = 'Images in time photographic collection. (Toledo Lucas County Public Library); Waterville (Ohio). History. Photographs.'
+subjectoregon = 'Images in time photographic collection. (Toledo Lucas County Public Library); Oregon (Ohio). History. Photographs.'
+subjectmaumee = 'Images in time photographic collection. (Toledo Lucas County Public Library); Maumee (Ohio). History. Photographs.'
+subjectsylvania = 'Images in time photographic collection. (Toledo Lucas County Public Library); Sylvania (Ohio). History. Photographs.'
+subjecttoledo = 'Images in time photographic collection. (Toledo Lucas County Public Library); Toledo (Ohio). History. Photographs.'
+subjectnonlucascounty = 'Images in time photographic collection. (Toledo Lucas County Public Library)'
+semicolonspace = '; '
+
+for row in ws.iter_rows(min_row=minimumrow, min_col=minimumcol, max_row=maximumrow, max_col=maximumcol):
+    for cell in row:
+        print(iterationrow)
+        testvar = ws.cell(row=iterationrow, column=targetcol).value
+        placevar = ws.cell(row=iterationrow, column=placecol).value
+        if testvar == None:
+            if placevar == None:
+                ws.cell(row=iterationrow, column=targetcol).value = subjectnonlucascounty
+                print(ws.cell(row=iterationrow, column=targetcol).value)
+            elif placevar.find('Toledo (Ohio)') != -1:
+                ws.cell(row=iterationrow, column=targetcol).value = subjecttoledo
+                print(ws.cell(row=iterationrow, column=targetcol).value)
+            elif placevar.find('Sylvania (Ohio)') != -1:
+                ws.cell(row=iterationrow, column=targetcol).value = subjectsylvania
+                print(ws.cell(row=iterationrow, column=targetcol).value)
+            elif placevar.find('Maumee (Ohio)') != -1:
+                ws.cell(row=iterationrow, column=targetcol).value = subjectmaumee
+                print(ws.cell(row=iterationrow, column=targetcol).value)
+            elif placevar.find('Oregon (Ohio)') != -1:
+                ws.cell(row=iterationrow, column=targetcol).value = subjectoregon
+                print(ws.cell(row=iterationrow, column=targetcol).value)
+            elif placevar.find('Waterville (Ohio)') != -1:
+                ws.cell(row=iterationrow, column=targetcol).value = subjectwaterville
+                print(ws.cell(row=iterationrow, column=targetcol).value)
+            elif placevar.find('Holland (Ohio)') != -1:
+                ws.cell(row=iterationrow, column=targetcol).value = subjectholland
+                print(ws.cell(row=iterationrow, column=targetcol).value)
+            else:
+                ws.cell(row=iterationrow, column=targetcol).value = subjectnonlucascounty
+                print(ws.cell(row=iterationrow, column=targetcol).value)
+        else:
+            if placevar == None:    
+                ws.cell(row=iterationrow, column=targetcol).value = testvar + semicolonspace + subjectnonlucascounty
+                print(ws.cell(row=iterationrow, column=targetcol).value)
+            elif placevar.find('Toledo') != -1:
+                ws.cell(row=iterationrow, column=targetcol).value = testvar + semicolonspace + subjecttoledo
+                print(ws.cell(row=iterationrow, column=targetcol).value)
+            elif placevar.find('Sylvania (Ohio)') != -1:
+                ws.cell(row=iterationrow, column=targetcol).value = testvar + semicolonspace + subjectsylvania
+                print(ws.cell(row=iterationrow, column=targetcol).value)
+            elif placevar.find('Maumee (Ohio)') != -1:
+                ws.cell(row=iterationrow, column=targetcol).value = testvar + semicolonspace + subjectmaumee
+                print(ws.cell(row=iterationrow, column=targetcol).value)
+            elif placevar.find('Oregon (Ohio)') != -1:
+                ws.cell(row=iterationrow, column=targetcol).value = testvar + semicolonspace + subjectoregon
+                print(ws.cell(row=iterationrow, column=targetcol).value)
+            elif placevar.find('Waterville (Ohio)') != -1:
+                ws.cell(row=iterationrow, column=targetcol).value = testvar + semicolonspace + subjectwaterville
+                print(ws.cell(row=iterationrow, column=targetcol).value)
+            elif placevar.find('Holland (Ohio)') != -1:
+                ws.cell(row=iterationrow, column=targetcol).value = testvar + semicolonspace + subjectholland
+                print(ws.cell(row=iterationrow, column=targetcol).value)
+            else:
+                ws.cell(row=iterationrow, column=targetcol).value = testvar + semicolonspace + subjectnonlucascounty
+                print(ws.cell(row=iterationrow, column=targetcol).value)
+        iterationrow = iterationrow + 1
+wb.save('aalh_iit_buildings_04.xlsx')
---
+++ aalh_iit_buildings_04/populate-subject-column.py
@@ -0,0 +1,56 @@
+from openpyxl import load_workbook
+
+filename = 'aalh_iit_buildings_04.xlsx'
+wb = load_workbook(filename)
+ws = wb['Metadata Template']
+
+minimumcol = 8
+maximumcol = 8
+minimumrow = 7
+maximumrow = 529
+
+iterationrow = 7
+descol = 8
+subcol = 9
+
+for row in ws.iter_rows(min_row=minimumrow, min_col=minimumcol, max_row=maximumrow, max_col=maximumcol):
+    testvar = ws.cell(row=iterationrow, column=descol).value
+    for cell in row:
+        print(iterationrow)
+        if testvar.find('house') != -1:
+            if testvar.find('warehouse') != -1:
+                #print('Warehouse')
+                continue
+            elif testvar.find('courthouse')!= -1:
+                #print('Courthouse')
+                continue
+            else:
+                ws.cell(row=iterationrow, column=subcol).value = 'Dwellings. Photographs.'
+                print(ws.cell(row=iterationrow, column=subcol).value)
+        elif testvar.find('House') != -1:
+            if testvar.find('Warehouse') != -1:
+                continue
+                # print('Warehouse')
+            elif testvar.find('Courthouse')!= -1:
+                #print('Courthouse')
+                continue
+            else:
+                ws.cell(row=iterationrow, column=subcol).value = 'Dwellings. Photographs.'
+                print(ws.cell(row=iterationrow, column=subcol).value)
+        elif testvar.find('dwelling') != -1:
+            ws.cell(row=iterationrow, column=subcol).value = 'Dwellings. Photographs.'
+            print(ws.cell(row=iterationrow, column=subcol).value)
+        elif testvar.find('Dwelling') != -1:
+            ws.cell(row=iterationrow, column=subcol).value = 'Dwellings. Photographs.'
+            print(ws.cell(row=iterationrow, column=subcol).value)
+        elif testvar.find('Church') != -1:
+            ws.cell(row=iterationrow, column=subcol).value = 'Churches. Photographs.'
+            print(ws.cell(row=iterationrow, column=subcol).value)
+        elif testvar.find('church') != -1:
+            ws.cell(row=iterationrow, column=subcol).value = 'Churches. Photographs.'
+            print(ws.cell(row=iterationrow, column=subcol).value)
+        else:
+            ws.cell(row=iterationrow, column=subcol).value = 'Buildings. Photographs.'
+            print(ws.cell(row=iterationrow, column=subcol).value)
+    iterationrow = iterationrow + 1
+wb.save('aalh_iit_buildings_04.xlsx')
