81060972163a5135575066e28020f58af7974efc
johndewees <john.dewees@gmail.com>
2021-02-18 16:55:48 -0500

    Add files via upload

---
+++ aalh_iit_peopleportraits_006/cleanup-originaldate-column.py
@@ -0,0 +1,54 @@
+from openpyxl import load_workbook
+import re
+
+filename = 'aalh_iit_peopleportraits_006.xlsx'
+wb = load_workbook(filename)
+ws = wb['Metadata Template']
+
+minimumcol = 15
+maximumcol = 15
+minimumrow = 7
+maximumrow = 587
+
+iterationrow = 7
+targetcol = 15
+
+for row in ws.iter_rows(min_row=minimumrow, min_col=minimumcol, max_row=maximumrow, max_col=maximumcol):
+    for cell in row:
+        print(iterationrow)
+        testvar = ws.cell(row=iterationrow, column=targetcol).value
+        print(testvar)
+        cleandate = None
+        approx = 'approximately '
+        try:
+            if testvar == None:
+                ws.cell(row=iterationrow, column=targetcol).value = ''
+            elif testvar.startswith('c'):
+                cleandate = re.findall('\d\d\d\d', testvar)
+                ws.cell(row=iterationrow, column=targetcol).value = approx + cleandate[0]
+            elif testvar.startswith('C'):
+                cleandate = re.findall('\d\d\d\d', testvar)
+                ws.cell(row=iterationrow, column=targetcol).value = approx + cleandate[0]
+            elif testvar.startswith('a'):
+                cleandate = re.findall('\d\d\d\d', testvar)
+                ws.cell(row=iterationrow, column=targetcol).value = approx + cleandate[0]
+            elif testvar.endswith('?'):
+                cleandate = testvar[:-1]
+                ws.cell(row=iterationrow, column=targetcol).value = approx + cleandate
+            elif testvar.find('-') != -1:
+                cleandate = testvar
+                ws.cell(row=iterationrow, column=targetcol).value = cleandate
+            elif testvar.find(',') != -1:
+                cleandate = testvar
+                ws.cell(row=iterationrow, column=targetcol).value = cleandate
+            elif testvar.find('/') != -1:
+                cleandate = testvar
+                ws.cell(row=iterationrow, column=targetcol).value = cleandate
+            else :
+                cleandate = re.findall('\d\d\d\d', testvar)
+                ws.cell(row=iterationrow, column=targetcol).value = cleandate[0]
+            print(ws.cell(row=iterationrow, column=targetcol).value)
+        except:
+            print('STATUS = PROBLEM')
+        iterationrow = iterationrow + 1
+wb.save('aalh_iit_peopleportraits_006.xlsx')
