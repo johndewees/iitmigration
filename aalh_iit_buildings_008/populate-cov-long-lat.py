2a1c6c5e15b1b20b70397ef398f6000397b7f563
johndewees <john.dewees@gmail.com>
2021-03-16 16:01:25 -0400

    Add files via upload

---
+++ aalh_iit_buildings_008/populate-cov-long-lat.py
@@ -0,0 +1,38 @@
+from openpyxl import load_workbook
+import re
+
+filename = 'aalh_iit_buildings_008.xlsx'
+wb = load_workbook(filename)
+ws = wb['Metadata Template']
+
+minimumcol = 8
+maximumcol = 8
+minimumrow = 7
+maximumrow = 517
+
+iterationrow = 7
+desccol = 8
+covcol = 10
+latcol = 11
+longcol = 12
+
+for row in ws.iter_rows(min_row=minimumrow, min_col=minimumcol, max_row=maximumrow, max_col=maximumcol):
+    testvar = ws.cell(row=iterationrow, column=desccol).value
+    for cell in row:
+        print(iterationrow)
+        if testvar == None:
+            continue
+        elif testvar.find('Old Main') != -1:
+            continue
+        elif testvar.find('Main Library') != -1:
+            ws.cell(row=iterationrow, column=covcol).value = '325 Michigan Street'
+            ws.cell(row=iterationrow, column=latcol).value = '41.654358'
+            ws.cell(row=iterationrow, column=longcol).value = '-83.539662'
+            print('MAIN LIBRARY')
+        elif testvar.find('West Toledo') != -1:
+            ws.cell(row=iterationrow, column=covcol).value = '1320 West Sylvania Avenue'
+            ws.cell(row=iterationrow, column=latcol).value = '41.69297'
+            ws.cell(row=iterationrow, column=longcol).value = '-83.572885'
+            print('WEST TOLEDO')
+    iterationrow = iterationrow + 1
+#wb.save("aalh_iit_parksnature_001.xlsx")
