2836563d2f2ad50019fe594ebf667b14f1d0c0f4
johndewees <john.dewees@gmail.com>
2021-03-23 18:30:22 -0400

    Add files via upload

---
+++ aalh_iit_peopleportraits_010/download-image-files.py
@@ -0,0 +1,26 @@
+from openpyxl import load_workbook
+import urllib.request
+
+filename = 'aalh_iit_peopleportraits_010.xlsx'
+wb = load_workbook(filename)
+ws = wb['Metadata Template']
+
+minimumcol = 2
+maximumcol = 50
+minimumrow = 7
+maximumrow = 495
+
+targetcol = 48
+iterationrow = 7
+
+for row in ws.iter_rows(min_row=minimumrow, min_col=targetcol, max_row=maximumrow, max_col=targetcol):
+    for cell in row:
+        testvar = ws.cell(row=iterationrow, column=targetcol).value
+        if testvar == 'SKIP':
+            continue
+        else:
+            downloadurl = ws.cell(row=iterationrow, column=48).value
+            downloadfilename = ws.cell(row=iterationrow, column=43).value
+            download = urllib.request.urlretrieve(downloadurl, downloadfilename)
+    iterationrow = iterationrow + 1
+print('*****IMAGES DOWNLOADED*****')
---
+++ aalh_iit_peopleportraits_010/populate-iso8601-nonamerdate.py
@@ -0,0 +1,56 @@
+from openpyxl import load_workbook
+import re
+
+filename = 'aalh_iit_peopleportraits_010.xlsx'
+wb = load_workbook(filename)
+ws = wb['Metadata Template']
+
+minimumcol = 15
+maximumcol = 15
+minimumrow = 7
+maximumrow = 499
+
+iterationrow = 7
+targetcol = 15
+isostandardcol = 16
+
+for row in ws.iter_rows(min_row=minimumrow, min_col=minimumcol, max_row=maximumrow, max_col=maximumcol):
+    for cell in row:
+        testvar = ws.cell(row=iterationrow, column=targetcol).value
+        isovalue = None
+        try:
+            if testvar == None:
+                ws.cell(row=iterationrow, column=isostandardcol).value = None
+            elif testvar.find('1945-46') != -1:
+                isovalue = '1945; 1946'
+                ws.cell(row=iterationrow, column=targetcol).value = '1945-1946'
+                ws.cell(row=iterationrow, column=isostandardcol).value = isovalue
+            elif testvar.find('1892-3') != -1:
+                isovalue = '1892; 1893'
+                ws.cell(row=iterationrow, column=targetcol).value = '1892-1893'
+                ws.cell(row=iterationrow, column=isostandardcol).value = isovalue
+            elif testvar.find('-') != -1:
+                isovalue = testvar
+                if isovalue.endswith('?'):
+                    isovalue = isovalue[:-1]
+                ws.cell(row=iterationrow, column=isostandardcol).value = isovalue
+            elif testvar.find(',') != -1:
+                print(iterationrow,'|',testvar,'|','STATUS = AMERICAN DATE')
+            elif testvar.find('/') != -1:
+                print(iterationrow,'|',testvar,'|','STATUS = AMERICAN DATE')
+            else :
+                isovalue = re.findall('\d\d\d\d', testvar)
+                ws.cell(row=iterationrow, column=isostandardcol).value = isovalue[0]
+        except:
+            print(iterationrow,'|',testvar,'|','STATUS = PROBLEM')
+        print(iterationrow,'|',testvar,'|',ws.cell(row=iterationrow, column=isostandardcol).value)
+    for cell in row:
+        testvar2 = ws.cell(row=iterationrow, column=isostandardcol).value
+        if testvar2 == None:
+            continue
+        elif testvar2.endswith('?'):
+            testvar3 = testvar2[:-1]
+            ws.cell(row=iterationrow, column=isostandardcol).value = testvar3
+            print(iterationrow,'| TRIMMED ?')
+    iterationrow = iterationrow + 1
+wb.save('aalh_iit_peopleportraits_010.xlsx')
