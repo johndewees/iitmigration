bf0573b79eacfa4c5f9c6666429f0a2dfb83aa3e
johndewees <john.dewees@gmail.com>
2021-03-25 15:36:15 -0400

    Add files via upload

---
+++ aalh_iit_tlcpl_002/populate-iso8601-nonamerdate.py
@@ -0,0 +1,40 @@
+from openpyxl import load_workbook
+import re
+
+filename = 'aalh_iit_tlcpl_002.xlsx'
+wb = load_workbook(filename)
+ws = wb['Metadata Template']
+
+minimumcol = 15
+maximumcol = 15
+minimumrow = 7
+maximumrow = 141
+
+iterationrow = 7
+targetcol = 15
+isostandardcol = 16
+
+for row in ws.iter_rows(min_row=minimumrow, min_col=minimumcol, max_row=maximumrow, max_col=maximumcol):
+    for cell in row:
+        testvar = ws.cell(row=iterationrow, column=targetcol).value
+        isovalue = None
+        try:
+            if testvar == None:
+                ws.cell(row=iterationrow, column=isostandardcol).value = None
+            elif testvar.find('-') != -1:
+                isovalue = testvar
+                if isovalue.endswith('?'):
+                    isovalue = isovalue[:-1]
+                ws.cell(row=iterationrow, column=isostandardcol).value = isovalue
+            elif testvar.find(',') != -1:
+                continue
+            elif testvar.find('/') != -1:
+                continue
+            else :
+                isovalue = re.findall('\d\d\d\d', testvar)
+                ws.cell(row=iterationrow, column=isostandardcol).value = isovalue[0]
+        except:
+            continue
+    print(iterationrow,'|',testvar,'|',ws.cell(row=iterationrow, column=isostandardcol).value)
+    iterationrow = iterationrow + 1
+wb.save('aalh_iit_tlcpl_002.xlsx')
---
+++ aalh_iit_tlcpl_002/populate-subject-column.py
@@ -0,0 +1,28 @@
+from openpyxl import load_workbook
+
+filename = 'aalh_iit_tlcpl_002.xlsx'
+wb = load_workbook(filename)
+ws = wb['Metadata Template']
+
+minimumcol = 8
+maximumcol = 8
+minimumrow = 7
+maximumrow = 140
+
+iterationrow = 7
+descol = 8
+subcol = 9
+
+for row in ws.iter_rows(min_row=minimumrow, min_col=minimumcol, max_row=maximumrow, max_col=maximumcol):
+    testvar = ws.cell(row=iterationrow, column=descol).value
+    for cell in row:
+        if testvar.find('Dwelling') != -1:
+            ws.cell(row=iterationrow, column=subcol).value = 'Dwellings. Photographs.'
+        elif testvar.find('dwelling') != -1:
+            ws.cell(row=iterationrow, column=subcol).value = 'Dwellings. Photographs.'
+        else:
+            ws.cell(row=iterationrow, column=subcol).value = 'Buildings. Photographs.'
+    print(iterationrow,'|',ws.cell(row=iterationrow, column=subcol).value)
+    iterationrow = iterationrow + 1
+print('*****COMPLETED*****')
+#wb.save('aalh_iit_tlcpl_002.xlsx')
