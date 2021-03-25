305eaaedeb60ca8e5186494eaf28fa1fa54c1aa2
johndewees <john.dewees@gmail.com>
2021-03-25 13:32:08 -0400

    Add files via upload

---
+++ aalh_iit_samjones_001/cleanup-originaldate-column.py
@@ -0,0 +1,52 @@
+from openpyxl import load_workbook
+import re
+
+filename = 'aalh_iit_samjones_001.xlsx'
+wb = load_workbook(filename)
+ws = wb['Metadata Template']
+
+minimumcol = 15
+maximumcol = 15
+minimumrow = 7
+maximumrow = 37
+
+iterationrow = 7
+targetcol = 15
+
+for row in ws.iter_rows(min_row=minimumrow, min_col=minimumcol, max_row=maximumrow, max_col=maximumcol):
+    for cell in row:
+        testvar = ws.cell(row=iterationrow, column=targetcol).value
+        cleandate = None
+        approx = 'approximately '
+        try:
+            if testvar == None:
+                ws.cell(row=iterationrow, column=targetcol).value = ''
+            elif testvar.startswith('c'):
+                cleandate = re.findall('\d\d\d\d', testvar)
+                ws.cell(row=iterationrow, column=targetcol).value = approx + cleandate[0]
+            elif testvar.startswith('C'):
+                cleandate = re.findall('\d\d\d\d', testvar)
+                ws.cell(row=iterationrow, column=targetcol).value = approx + cleandate[0]
+            elif testvar.startswith('a'):
+                cleandate = re.findall('\d\d\d\d', testvar)
+                ws.cell(row=iterationrow, column=targetcol).value = approx + cleandate[0]
+            elif testvar.endswith('?'):
+                cleandate = testvar[:-1]
+                ws.cell(row=iterationrow, column=targetcol).value = approx + cleandate
+            elif testvar.find('-') != -1:
+                cleandate = testvar
+                ws.cell(row=iterationrow, column=targetcol).value = cleandate
+            elif testvar.find(',') != -1:
+                cleandate = testvar
+                ws.cell(row=iterationrow, column=targetcol).value = cleandate
+            elif testvar.find('/') != -1:
+                cleandate = testvar
+                ws.cell(row=iterationrow, column=targetcol).value = cleandate
+            else :
+                cleandate = re.findall('\d\d\d\d', testvar)
+                ws.cell(row=iterationrow, column=targetcol).value = cleandate[0]
+            print(iterationrow,'|',testvar,'|',ws.cell(row=iterationrow, column=targetcol).value)
+        except:
+            print(iterationrow,'|',testvar,'|','STATUS = PROBLEM')
+    iterationrow = iterationrow + 1
+wb.save('aalh_iit_samjones_001.xlsx')
---
+++ aalh_iit_samjones_001/debug-find-duplicates.py
@@ -0,0 +1,47 @@
+from openpyxl import load_workbook
+
+filename = 'aalh_iit_samjones_001.xlsx'
+wb = load_workbook(filename)
+ws = wb['Metadata Template']
+
+minimumcol = 43
+maximumcol = 43
+minimumrow = 7
+maximumrow = 37
+
+iterationrow = 7
+identifiercol = 25
+filenamecol = 43
+
+countfilename = dict()
+countidentifier = dict()
+
+for row in ws.iter_rows(min_row=minimumrow, min_col=minimumcol, max_row=maximumrow, max_col=maximumcol):
+    for cell in row:
+        #print(iterationrow)
+        testvar1 = ws.cell(row=iterationrow, column=filenamecol).value
+        #print(testvar1)
+        if testvar1 not in countfilename:
+            countfilename[testvar1] = 1
+        else:
+            countfilename[testvar1] = countfilename[testvar1] + 1
+    for cell in row:
+        testvar2 = ws.cell(row=iterationrow, column=identifiercol).value
+        #print(testvar2)
+        try:
+            if testvar2 not in countfilename:
+                countidentifier[testvar2] = 1
+            else:
+                countidentifier[testvar2] = countidentifier[testvar2] + 1
+        except:
+            continue
+    iterationrow = iterationrow + 1
+for file1 in countfilename:
+    if countfilename[file1] > 1:
+        print('Duplicate File Name:',file1, countfilename[file1])
+for file2 in countidentifier:
+    if countidentifier[file2] > 1:
+        print('Duplicate Identifier:',file2, countidentifier[file2])
+print('*Duplicate Check Completed*')
+print(countfilename)
+print(countidentifier)
